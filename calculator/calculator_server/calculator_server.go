621f73e658e7ea5e9a3aaf8747e81543e2abdd95
Pushpan <pushpan.nresht@go-mmt.com>
2020-06-21 16:02:09 +0530

    gRPC Framework - Adding refelection for CLI

--- calculator/calculator_server/calculator_server.go
+++ calculator/calculator_server/calculator_server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pushpan/gRPC-GO/calculator/calculatorpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 	"io"
 	"log"
@@ -118,6 +119,8 @@ func main() {
 	}
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
